eb09d9addde214af2599c696d18d50f178b4f5ce
doaajamal_0 <obaiddoaa9@gmail.com>
2023-09-06 19:22:17 +0300

    Test

--- app/src/main/java/com/example/takkah_an/MainActivity.java
+++ app/src/main/java/com/example/takkah_an/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.takkah_an;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        logo = findViewById(R.id.logo);
+        YoYo.with(Techniques.RotateIn)
+                .duration(1000)
+                .playOn(findViewById(R.id.logo));
+
+        logo = findViewById(R.id.logo);
+        YoYo.with(Techniques.Flash)
+                .duration(1000)
+                .playOn(findViewById(R.id.logo));
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this,onbording1.class);
+                startActivity(i);
+            }
+        },4000);
+
     }
 }
---
+++ app/src/main/java/com/example/takkah_an/createPassword.java
@@ -0,0 +1,14 @@
+package com.example.takkah_an;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class createPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_password);
+    }
+}
